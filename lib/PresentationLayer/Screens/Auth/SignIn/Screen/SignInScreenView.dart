cbef060ad352c177780a6fcc60cdcc311fd46bfb
aleksandr.orlov <aleksandr.orlov@deskera.com>
2020-10-01 22:22:11 +0800

    Implemented go to next field, show/hide password at sign in screen.

--- lib/PresentationLayer/Screens/Auth/SignIn/Screen/SignInScreenView.dart
+++ lib/PresentationLayer/Screens/Auth/SignIn/Screen/SignInScreenView.dart
@@ -27,9 +27,13 @@ class SignInScreenView extends StatefulWidget {
 
 class SignInScreenViewState extends State<SignInScreenView> {
 
+  // Dependencies
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _passwordFocus = FocusNode();
 
+  // Data
+  bool _passwordSecure = true;
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +51,7 @@ class SignInScreenViewState extends State<SignInScreenView> {
           padding: EdgeInsets.symmetric(horizontal: 18),
           child: Column(
             children: [
-              makeLoginTextField(),
+              makeLoginTextField(context),
               makePasswordTextField(),
               makeSignUpButton(),
               makeSignInButton(context),
@@ -58,31 +62,57 @@ class SignInScreenViewState extends State<SignInScreenView> {
     );
   }
 
-  Widget makeLoginTextField() {
+  // Login
+  Widget makeLoginTextField(BuildContext context) {
     return Container(
       child: TextInput(
         controller: _loginController,
         placeholder: 'Login',
         errorText: widget.loginError,
+        textInputAction: TextInputAction.next,
         onChanged: (text) { widget.onChangeLogin(); },
+        onSubmitted: (text) { _makePasswordInFocus(context); },
       ),
       margin: EdgeInsets.only(top: 200),
     );
   }
 
+  // Password
   Widget makePasswordTextField() {
     return Container(
       child: TextInput(
         controller: _passwordController,
         placeholder: 'Password',
-        obscureText: true,
+        obscureText: _passwordSecure,
         errorText: widget.passwordError,
+        focusNode: _passwordFocus,
+        suffixIcon: _buildPasswordVisibilityButton(),
         onChanged: (text) { widget.onChangePassword(); },
+        onSubmitted: (text) { _onSignIn(); },
       ),
       margin: EdgeInsets.only(top: 6),
     );
   }
 
+  Widget _buildPasswordVisibilityButton() {
+    return GestureDetector(
+      child: Icon(
+        _passwordSecure ? Icons.visibility : Icons.visibility_off,
+        color: appearance.text.secondary,
+      ),
+      onTap: _handleTapOnPasswordVisibilityButton,
+    );
+  }
+
+  void _handleTapOnPasswordVisibilityButton() {
+    setState(() { _passwordSecure = !_passwordSecure; });
+  }
+
+  void _makePasswordInFocus(BuildContext context) {
+    FocusScope.of(context).requestFocus(_passwordFocus);
+  }
+
+  // Sign up
   Widget makeSignUpButton() {
     return Container(
       child: Row(
@@ -99,16 +129,19 @@ class SignInScreenViewState extends State<SignInScreenView> {
     );
   }
 
+  // Sign in
   Widget makeSignInButton(BuildContext context) {
     return FlatButton(
-      onPressed: () {
-        this.widget.onSignIn(_loginController.text, _passwordController.text);
-      },
+      onPressed: _onSignIn,
       child: Text('Sign In'),
       color: appearance.button.primary.background,
       textColor: appearance.button.primary.title,
       height: 50,
       minWidth: (MediaQuery.of(context).size.width) - 18 * 2,
     );
+  }
+
+  void _onSignIn() {
+    this.widget.onSignIn(_loginController.text, _passwordController.text);
   }
 }
